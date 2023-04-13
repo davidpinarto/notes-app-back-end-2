b4b7fdbe59dda7e48329898c551e653775b3c3e9
David Pinarto <davidpinarto90@gmail.com>
2023-04-13 19:38:41 +0800

    local storage implemented

---
+++ src/api/uploads/handler.js
@@ -0,0 +1,29 @@
+/* eslint-disable no-underscore-dangle */
+const autoBind = require('auto-bind');
+
+class UploadsHandler {
+  constructor(service, validator) {
+    this._service = service;
+    this._validator = validator;
+
+    autoBind(this); // mem-bind nilai this untuk seluruh method sekaligus
+  }
+
+  async postUploadImageHandler(request, h) {
+    const { data } = request.payload;
+    this._validator.validateImageHeaders(data.hapi.headers);
+
+    const filename = await this._service.writeFile(data, data.hapi);
+
+    const response = h.response({
+      status: 'success',
+      data: {
+        fileLocation: `http://${process.env.HOST}:${process.env.PORT}/upload/images/${filename}`,
+      },
+    });
+    response.code(201);
+    return response;
+  }
+}
+
+module.exports = UploadsHandler;
